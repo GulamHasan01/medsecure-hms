7d072013513a304c71b1be0fa1f88cdfb14e7d10
Gulam Hasan <ghulamhasan665@gmail.com>
2026-03-02 11:54:41 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/hasan/medsecure_hms/MedsecureHmsApplication.java
@@ -0,0 +1,13 @@
+package com.hasan.medsecure_hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedsecureHmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedsecureHmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hasan/medsecure_hms/MedsecureHmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasan.medsecure_hms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedsecureHmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
